97e70c5fb0df9a2aae99152ee6d4d36846da7633
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-05-03 17:21:25 +0200

    email confirmation done

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -5,7 +5,7 @@ const userModel = require('../models/userModel');
 emailRouter.get('/confirm/:stamp', async (req, res, next) => {
   try {
     const user = await userModel.findOneAndUpdate({temporaryToken: req.params.stamp}, {$set: {isConfirmed: true}, $unset: {temporaryToken: ''}});
-    user ? res.status(202).json({message: `User with username ${user.userName} is now confirmed`}) : res.status(404).json({message: 'User was not found'})
+    user ? res.redirect('https://www.google.de') : res.status(404).json({message: 'User was not found'})
   }catch{
     next(error);
   }
